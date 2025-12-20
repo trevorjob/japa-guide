ff5aacb78ad4a9932b4cc733684fa6de6c5d2b76
kumdan job <redeks123456@gmail.com>
2025-12-20 10:51:06 +0100

    next js upgrade

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Turbopack will use tsconfig.json paths for aliasing
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
